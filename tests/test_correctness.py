48f64a87eb051701383da777d3b446fea6d39a8d
realkevinkwon <kevinkwonmail@gmail.com>
2023-02-09 16:31:59 -0500

    move test files

---
+++ tests/test_correctness.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(1, '../Run/URL_Fields')
+import Correctness
+
+def test_get_downlaods():
+    pass
+
+def test_get_doc():
+    pass
+
+def test_get_stars():
+    pass
+
+def test_get_issues():
+    pass
+
+def test_get_pr():
+    pass
+
+def test_get_Resp_Maintainer():
+    pass
