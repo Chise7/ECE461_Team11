cc1f1c5b2e803dd64634fef651aa815155a24a8d
Chise7 <89486943+Chise7@users.noreply.github.com>
2023-02-08 21:39:26 -0500

    Install Updates

--- Run/install.py
+++ Run/install.py
@@ -1,7 +1,11 @@
 import subprocess
 def run_install():
-    subprocess.run(["pip", "install", "PyGithub"])
-    subprocess.run(["pip", "install", "regex"])
+    subprocess.run(["pip", "install", "PyGithub", "-q"])
+    subprocess.run(["pip", "install", "regex","-q"])
+    subprocess.run(["pip", "install", "ndjson","-q"])
     # subprocess.run(["cargo", "add", "serde"])
     # subprocess.run(["cargo", "add", "reqwest"])
-    print("Install Complete.")
+    # print("Install Complete.")
+
+if __name__ == "__main__":
+    run_install()
