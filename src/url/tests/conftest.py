298a23febde40a7036049cb0655d00eee6e1ebfa
PC192 <89347315+PC192@users.noreply.github.com>
2023-02-12 13:25:29 -0500

    Update conftest.py

--- src/url/tests/conftest.py
+++ src/url/tests/conftest.py
@@ -11,7 +11,9 @@ VALID_TEST_CASES = [
     ("https://www.npmjs.com/package/express", "expressjs", "express"),
     ("https://www.npmjs.com/package/browserify", "browserify", "browserify"),
     ("https://github.com/cloudinary/cloudinary_npm ", "cloudinary", "cloudinary_npm"),
-    ("https://github.com/lodash/lodash", "lodash", "lodash")
+    ("https://github.com/lodash/lodash", "lodash", "lodash"),
+    ("https://github.com/PC192/ChubbyChecker", "PC192", "ChubbyChecker"), # Increase code coverage (missing items in valid repo)
+    ("https://github.com/apache/airflow", "apache", "airflow") # Increase code coverage (too many items in valid repo)
 ]
 INVALID_TEST_CASES = [
     ("https://www.github.com/lodash/lodash", "unga", "lodash"), # incorrect owner
