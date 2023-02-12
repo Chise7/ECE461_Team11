2244e1684824da21fdd2346c87a11f54dca649f5
PC192 <89347315+PC192@users.noreply.github.com>
2023-02-11 21:13:56 -0500

    Update test_ramp_up.py

--- src/url/tests/test_ramp_up.py
+++ src/url/tests/test_ramp_up.py
@@ -1,9 +1,9 @@
-import sys, pytest
+import sys, pytest, os
 #sys.path.append("..")
 from url import ramp_up
 
 USERNAME =  'PC192'
-TOKEN = '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'
+TOKEN = os.getenv('GITHUB_TOKEN')
 Testcases = [('Chise7', 'ECE461_Team11'),('words', 'double-metaphone'),('jonschlinkert', 'even'), ('SonarSource', 'chocolatey-packages'),('PSOPT', 'psopt'),('nullivex','nodist'),('cloudinary','cloudinary_npm'), ('lodash','lodash')]
 
 def test_ramp_up():
