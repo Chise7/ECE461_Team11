f87f36d839d7ef2de39070bf7b6ad18a9f5926ca
PC192 <89347315+PC192@users.noreply.github.com>
2023-02-11 20:18:02 -0500

    Update test_ramp_up.py

--- src/url/tests/test_ramp_up.py
+++ src/url/tests/test_ramp_up.py
@@ -1,15 +1,12 @@
-import os
-import pytest
-from url.ramp_up import *
+import sys, pytest
+#sys.path.append("..")
+from url import ramp_up
 
 USERNAME =  'PC192'
-TOKEN = os.getenv('insert token here')
-Testcases = [('Chise7', 'ECE461_Team11'),('words', 'double-metaphone'),('jonschlinkert', 'even-metaphone'),
-            ('SonarSource', 'chocolatey-packages'),('PSOPT', 'psopt'),('nullivex','nodist'),('cloudinary','cloudinary_npm'),
-            ('lodash','lodash')]
+TOKEN = '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'
+Testcases = [('Chise7', 'ECE461_Team11'),('words', 'double-metaphone'),('jonschlinkert', 'even'), ('SonarSource', 'chocolatey-packages'),('PSOPT', 'psopt'),('nullivex','nodist'),('cloudinary','cloudinary_npm'), ('lodash','lodash')]
 
-@pytest.mark.correct_ramp_up
 def test_ramp_up():
     for cases in Testcases:
-        validrampup = ramp_up(cases[0], cases[1], TOKEN)
+        validrampup = ramp_up.ramp_up(cases[0], cases[1], TOKEN)
         assert validrampup >= 0
