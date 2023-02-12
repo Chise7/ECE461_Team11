918009e946008d1eab460ba9848aa610a3cbfa07
PC192 <89347315+PC192@users.noreply.github.com>
2023-02-11 23:01:29 -0500

    Edit to remove token

--- src/url/tests/test_ramp_up.py
+++ src/url/tests/test_ramp_up.py
@@ -1,10 +1,8 @@
 import sys, pytest, os
-#sys.path.append("..")
 from url import ramp_up
 
 USERNAME =  'PC192'
-#TOKEN = os.getenv('GITHUB_TOKEN')
-TOKEN = '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'
+TOKEN = os.getenv('GITHUB_TOKEN')
 Testcases = [('Chise7', 'ECE461_Team11'),('words', 'double-metaphone'),('jonschlinkert', 'even'), ('apache', 'airflow'),('PSOPT', 'psopt'),('nullivex','nodist'),('cloudinary','cloudinary_npm'), ('PC192','ChubbyChecker')]
 
 def test_ramp_up():
